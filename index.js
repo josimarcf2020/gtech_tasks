4fc3ab314e688c9b84031ea566adbc33b5589f16
Josimar Ferreira <jcfcavile@gmail.com>
2025-05-03 17:03:35 -0300

    feature/pesquisa por titulo

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function popularTarefas(listaDetarefas) {
                                 <h5>${tarefa.titulo}</h5>
                                 <p>${tarefa.descricao}</p>
                                 <div class="actions">
-                                    <box-icon type='solid' name='trash' size='sm'></box-icon>
+                                    <box-icon class="delete-icon" type='solid' name='trash' size='sm' data-id='${tarefa.id}'></box-icon>
                                 </div>
                             </li>`;
         })
@@ -52,6 +52,43 @@ function persistirTarefa() {
         fecharModal();
         buscarTarefas();    
     })
-    
 }
 
+function deletarTarefa(id){
+    fetch(`http://localhost:3000/tarefas/${id}`, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert("Tarefa deletada com sucesso!");
+        buscarTarefas();
+    })
+}
+
+const lista = document.querySelector('#lista'); // Certifique-se que o seletor '#lista' corresponde ao seu elemento UL/OL
+if (lista) {
+    lista.addEventListener('click', function(event) {
+        const deleteButton = event.target.closest('.delete-icon'); // Verifica se o clique foi no ícone ou dentro dele
+        if (deleteButton) {
+            const tarefaId = deleteButton.getAttribute('data-id');
+            deletarTarefa(tarefaId);
+        }
+    });
+}
+
+function pesquisarTarefa(){
+    let lis = document.querySelectorAll("ul li");
+    if(busca.value.length > 0){
+        lis.forEach(li => {
+            if(!li.children[0].innerText.includes(busca.value)){
+                li.classList.add('oculto');
+            }else{
+                li.classList.remove('oculto');
+            }
+        })
+    }else {
+        lis.forEach(li => {
+            li.classList.remove('oculto');
+        })
+    }
+}
