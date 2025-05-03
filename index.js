9303aa3bbce174d2ed045d73926072da326ae87c
Josimar Ferreira <jcfcavile@gmail.com>
2025-05-03 11:45:25 -0300

    feature/Exibindo os dados mocados na tela de tarefas

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 function novaTarefa() {
     overlay.classList.add("active");
     criarTarefa.classList.add("active");
-    
 }
 
 function fecharModal() {
     overlay.classList.remove("active");
     criarTarefa.classList.remove("active");
+}
+
+function buscarTarefas() {
+    console.log("Buscando tarefas...");
+    fetch('http://localhost:3000/tarefas')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        popularTarefas(data);
+    })
+}
+buscarTarefas();
+
+function popularTarefas(listaDetarefas) {
+    if(listaDetarefas.length > 0) {
+        lista.innerHTML = "";
+        listaDetarefas.map(tarefa => {
+            lista.innerHTML += 
+                            `<li>
+                                <h5>${tarefa.titulo}</h5>
+                                <p>${tarefa.descricao}</p>
+                                <div class="actions">
+                                    <box-icon type='solid' name='trash' size='sm'></box-icon>
+                                </div>
+                            </li>`;
+        })
+    }
 }
